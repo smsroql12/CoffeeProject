4256cde96119a3887e7c3a581247ff031e0bad75
kys6879 <kys6879@naver.com>
2018-02-22 14:00:44 +0900

    1차 프론트 작업

---
+++ assets/js/login.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	var arrow = $(".arrow-up");
+	var form = $(".login-form");
+	var status = false;
+
+	$("#login").click(function(event){
+		event.preventDefault();
+		if(status == false) {
+			arrow.fadeIn();
+			form.fadeIn();
+			status = true;
+		} else {
+			arrow.fadeOut();
+			form.fadeOut();
+			status = false;
+		}
+	})
+})
