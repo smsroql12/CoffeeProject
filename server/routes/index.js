f2a739f05846e475ee1c7b2d6c06b3de10669afe
kys6879 <kys6879@naver.com>
2018-03-08 00:30:00 +0900

    정보수정 끝 (코드정리안되서 약간 복잡)

--- server/routes/index.js
+++ server/routes/index.js
@@ -68,63 +68,93 @@ router.post('/modifyInfo',function(req,res,next) {
   var addressO;
   var addressD;
   var email;
+  // 빈 배열 선언
   var modifys = [];
-  
+  // 회원정보 수정을 요청한 아이디의 정보를 가져옴
   var sql = `SELECT * FROM userInfo where id = ?`;
   var params = [sess.userid];
   console.log('수정요청 : 세션:'+params);
   conn.query(sql,params,function(err,rows,field) { // 먼저 기본 정보를 변수에 싹 넣어둔다음
     var i;
     var DBString = ["pw","name","dateOfbirth","email","gender","phone","postNum","addressN","addressO","addressD"];
+    // 빈 배열에 기존 회원정보를 넣음
+    console.log("------------------기존 유저 정보----------------");
     for(i=0; i<10; i++){
       modifys[i] = rows[0][DBString[i]];
+      console.log("기존 유저 정보 : " + modifys[i]);
     }
-    return ;        
-  })
-  var _password = req.body.pw;     
-  var _emailF = req.body.emailF;
-  var _emailB  = req.body;
-  var _emailS  = req.body.emailS
-  var _name  = req.body.name
-  var _gender = req.body.gender
-  var _dateOfbirth  = req.body.date
-  var _phone = req.body.phone
-  var _postNum  = req.body.postNum
-  var _addressN  = req.body.addressN
-  var _addressO = req.body.addressO
-  var _addressD = req.body.addressD
-  var _email; // 유저가 입력한 정보들
-
-  if(_emailS == "notSelected"){
-  _emailS = null;
-  }
-  else{ // 이메일을 선택했다면
+    // 문제없음
+    console.log("----------------------------------");
+    var _password = req.body.pw;     
+    var _emailF = req.body.emailF;
+    var _emailS  = req.body.emailS
+    var _name  = req.body.name
+    var _gender = req.body.gender
+    var _dateOfbirth  = req.body.date
+    var _phone = req.body.phone
+    var _postNum  = req.body.postNum
+    var _addressN  = req.body.addressN
+    var _addressO = req.body.addressO
+    var _addressD = req.body.addressD
+    var _email; // 유저가 입력한 정보들
+    
+    if(_emailS == "notSelected"){
+      _email = modifys[3]; 
+    }else{
       _email = _emailF+ "@" + _emailS;
-  }
-  var parameterEmail= String(_email); // 이메일을 문자열 화
-  var parameterString = ["pw",parameterEmail,"name","gender","date","phone","postNum","addressN","addressO","addressD"];
-  var _modifys = [_password,_name,_dateOfbirth,_email,_gender,_phone,_postNum,_addressN,_addressO,_addressD];
-
-  for(i =0; i<_modifys; i++){
-    res.send('for');
-    console.log('for문진입');
-        if(_modifys[i] == "" || _modifys[i] == null || _modifys[i] ==" " ){
-            console.log(i+"번째 항목은 수정이 되지 않았습니다."+_modifys[i]);
-            continue;
-        }  
-          // 수정을 했다면
+    }
+    _email= String(_email); // 이메일을 문자열 화
+    console.log("--------------------- 입력한 정보들 -----------------");
+    console.log("비번 : " + _password);
+    console.log("이름 : " + _name);
+    console.log("생년월일 : " + _dateOfbirth);
+    console.log("이메일 : "+_email);
+    console.log("성별 : " + _gender);
+    console.log("폰번호 : " + _phone);
+    console.log("우편번호 : " + _postNum);
+    console.log("신주소 : " + _addressN);
+    console.log("구주소 : " + _addressO);
+    console.log("상세주소 : " + _addressD);
+    console.log("--------------------------------------");
+    var parameterString = ["pw","name","date",_email,"gender","phone","postNum","addressN","addressO","addressD"];
+    var _modifys = [_password,_name,_dateOfbirth,_email,_gender,_phone,_postNum,_addressN,_addressO,_addressD];
+    //var DBString = ["pw","name","dateOfbirth","email","gender","phone","postNum","addressN","addressO","addressD"];
+    for(i =0; i<_modifys.length; i++){
+          if(_modifys[i] == "" || _modifys[i] == null || _modifys[i] ==" " ){
+              console.log(i+"번째 항목은 수정이 되지 않았습니다."+_modifys[i]);
+          }  
+            // 수정을 했다면
           else{
-            console.log(i+"번쨰 항목은 수정이 되었습니다!"+_modifys[i] + "으로 수정됨");
-            if(_modifys[1] != null){ // 이메일을 입력했다면
-              modifys[1] =  parameterEmail; // 문자열한 이메일을 집어넣음
-            }
+            if(i==3){
+              modifys[i] = _email;
+            }else{
               modifys[i] = req.body[parameterString[i]];
-            // s= s+modifys[i];
-            // s= s+"<br>";
-          }
-          s= s+modifys[i];  
-          s= s+"<br>";
-  } 
+            }
+              // _modifys 는 수정한 인풋텍스트를 모아놓은 배열
+              console.log(i+"번쨰 항목은 수정이 되었습니다!"+_modifys[i] + "으로 수정됨");
+            }
+            s = s+DBString[i]+ ":";
+            s= s+modifys[i];
+            s= s+"<br>";
+    }
+    // 수정 결과 표시
+    //res.send(s);
+    arr = new Array; 
+    sql = 'UPDATE userInfo SET pw=?, name=?, dateOfbirth=?, email=?, gender=?, phone=?,postNum=?,addressN=?,addressO=?,addressD=? WHERE id=?';
+    for(var i = 0 ; i<10; i++){
+      arr.push(modifys[i]);
+    }
+    arr.push(sess.userid)
+
+    conn.query(sql, arr, function(err, rows, fields){
+      if(err){
+        console.log(err);
+      } else {
+        res.send("정보 수정 완료 !");
+        console.log("정보 수정 완료 !");
+      }
+    });
+  })
 })
 
 router.post('/purchase',function(req,res,next) {
